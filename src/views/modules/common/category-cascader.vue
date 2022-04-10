6233a63cc1701b7798bed8540d59c3a52243ee04
zhangmingming <930137960@qq.com>
2022-04-10 16:44:46 +0800

    品牌管理-关联分类功能开发完毕

---
+++ src/views/modules/common/category-cascader.vue
@@ -0,0 +1,79 @@
+<template>
+  <!-- 品牌的级联搜索 抽成公共的组建以供复用 
+使用说明：
+1）、引入category-cascader.vue
+2）、语法：<category-cascader :catelogPath.sync="catelogPath"></category-cascader>
+    解释：
+      catelogPath：指定的值是cascader初始化需要显示的值，应该和父组件的catelogPath绑定;
+          由于有sync修饰符，所以cascader路径变化以后自动会修改父的catelogPath，这是结合子组件this.$emit("update:catelogPath",v);做的    
+-->
+  <div>
+    <el-cascader
+      filterable
+      clearable
+      placeholder="试试搜索：手机"
+      v-model="paths"
+      :options="categorys"
+      :props="setting"
+    ></el-cascader>
+  </div>
+</template>
+
+<script>
+//这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
+//例如：import 《组件名称》 from '《组件路径》';
+export default {
+  //component引入的参数要在这里进行注册才能使用
+  component: {},
+  //props是组件之间进行通信的用来接受父组件传来的值
+  props: {
+    catelogPath: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+  //存放数据的地方
+  data() {
+    return {
+      setting: {
+        value: "catId",
+        label: "name",
+        children: "children",
+      },
+      categorys: [],
+      paths: this.catelogPath,
+    };
+  },
+  watch: {
+    catelogPath(v) {
+      this.paths = this.catelogPath;
+    },
+    paths(v) {
+      this.$emit("update:catelogPath", v);
+      //还可以使用pubsub-js进行传值
+      this.PubSub.publish("catPath", v);
+    },
+  },
+  //方法合集
+  methods: {
+    getCategorys() {
+      this.$http({
+        url: this.$http.adornUrl("/product/category/list/tree"),
+        methods: "get",
+      }).then(({ data }) => {
+        this.categorys = data.data;
+      });
+    },
+  },
+
+  //生命周期-创建完成 可以访问当前this实例
+  created() {
+    this.getCategorys();
+  },
+};
+</script>
+
+<style scoped>
+</style>
--- src/views/modules/product/brand.vue
+++ src/views/modules/product/brand.vue
@@ -69,7 +69,7 @@
             :src="scope.row.logo"
             fit="fill"
           ></el-image> -->
-          <img :src="scope.row.logo" style="width: 100px; height: 80px">
+          <img :src="scope.row.logo" style="width: 100px; height: 80px" />
         </template>
       </el-table-column>
       <el-table-column
@@ -115,10 +115,16 @@
         fixed="right"
         header-align="center"
         align="center"
-        width="150"
+        width="250"
         label="操作"
       >
         <template slot-scope="scope">
+          <el-button
+            type="text"
+            size="small"
+            @click="updateCatelogHandle(scope.row.brandId)"
+            >关联分类</el-button
+          >
           <el-button
             type="text"
             size="small"
@@ -150,33 +156,132 @@
       ref="addOrUpdate"
       @refreshDataList="getDataList"
     ></add-or-update>
+
+    <el-dialog
+      title="关联分类"
+      :visible.sync="cateRelationDialogVisible"
+      width="30%"
+    >
+      <el-popover placement="right-end" v-model="popCatelogSelectVisible">
+        <category-cascader :catelogPath.sync="catelogPath"></category-cascader>
+        <div style="text-align: right; margin: 0">
+          <el-button
+            size="mini"
+            type="text"
+            @click="popCatelogSelectVisible = false"
+            >取消</el-button
+          >
+          <el-button size="mini" type="primary" @click="addCatelogSelect"
+            >确定</el-button
+          >
+        </div>
+        <el-button slot="reference">新增关联</el-button>
+      </el-popover>
+      <el-table :data="cateRelationTableData" style="width: 100%">
+        <el-table-column prop="id" label="#"></el-table-column>
+        <el-table-column prop="brandName" label="品牌名"></el-table-column>
+        <el-table-column prop="catelogName" label="分类名"></el-table-column>
+        <el-table-column
+          fixed="right"
+          header-align="center"
+          align="center"
+          label="操作"
+        >
+          <template slot-scope="scope">
+            <el-button
+              type="text"
+              size="small"
+              @click="deleteCateRelationHandle(scope.row.id, scope.row.brandId)"
+              >移除</el-button
+            >
+          </template>
+        </el-table-column>
+      </el-table>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="cateRelationDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="cateRelationDialogVisible = false"
+          >确 定</el-button
+        >
+      </span>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 import AddOrUpdate from "./brand-add-or-update";
+import CategoryCascader from "../common/category-cascader";
 export default {
   data() {
     return {
       dataForm: {
         key: "",
       },
+      brandId: 0,
+      catelogPath: [],
       dataList: [],
+      cateRelationTableData: [],
       pageIndex: 1,
       pageSize: 10,
       totalPage: 0,
       dataListLoading: false,
       dataListSelections: [],
       addOrUpdateVisible: false,
+      cateRelationDialogVisible: false,
+      popCatelogSelectVisible: false,
     };
   },
+  //把导入的组件注册在这里
   components: {
     AddOrUpdate,
+    CategoryCascader,
   },
   activated() {
     this.getDataList();
   },
   methods: {
+    addCatelogSelect() {
+      //{"brandId":1,"catelogId":2}
+      this.popCatelogSelectVisible = false;
+      this.$http({
+        url: this.$http.adornUrl("/product/categorybrandrelation/save"),
+        method: "post",
+        data: this.$http.adornData(
+          {
+            brandId: this.brandId,
+            catelogId: this.catelogPath[this.catelogPath.length - 1],
+          },
+          false
+        ),
+      }).then(({ data }) => {
+        this.getCateRelation();
+      });
+    },
+
+    deleteCateRelationHandle(id, brandId) {
+      this.$http({
+        url: this.$http.adornUrl("/product/categorybrandrelation/delete"),
+        method: "post",
+        data: this.$http.adornData([id], false),
+      }).then(({ data }) => {
+        this.getCateRelation();
+      });
+    },
+    updateCatelogHandle(brandId) {
+      this.cateRelationDialogVisible = true;
+      this.brandId = brandId;
+      this.getCateRelation();
+    },
+    getCateRelation() {
+      this.$http({
+        url: this.$http.adornUrl("/product/categorybrandrelation/catelog/list"),
+        method: "get",
+        params: this.$http.adornParams({
+          brandId: this.brandId,
+        }),
+      }).then(({ data }) => {
+        this.cateRelationTableData = data.data;
+      });
+    },
     // 获取数据列表
     getDataList() {
       this.dataListLoading = true;
@@ -257,7 +362,7 @@ export default {
         });
       });
     },
-    
+
     //更新品牌状态
     updateBrandStatus(data) {
       console.log("最新状态", data);
