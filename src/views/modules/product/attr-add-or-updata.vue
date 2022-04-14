0f99fdfbcbc01d868adcb7c664c18d69e3c11432
zhangmingming <930137960@qq.com>
2022-04-14 20:09:54 +0800

    开发完毕attr-add-or-updata组件

--- src/views/modules/product/attr-add-or-updata.vue
+++ src/views/modules/product/attr-add-or-updata.vue
@@ -1,18 +1,25 @@
 <template>
+  <!-- 点击新增或者修改的时候，弹出一个对话框  所以 最外层先放一个<el-dialog></el-dialog>-->
   <el-dialog
     :title="!dataForm.id ? '新增' : '修改'"
     :close-on-click-modal="false"
     :visible.sync="visible"
     @closed="dialogClose"
   >
-    <el-form :model="dataForm" :rules="dataRule" ref="dataForm" label-width="120px">
-      <!--       @keyup.enter.native="dataFormSubmit()" -->
+    <!-- <el-dialog>里面放一个form表单 ref 制定验证规则-->
+    <el-form
+      :model="dataForm"
+      :rules="dataRule"
+      ref="dataForm"
+      label-width="120px"
+    >
       <el-form-item label="属性名" prop="attrName">
         <el-input v-model="dataForm.attrName" placeholder="属性名"></el-input>
       </el-form-item>
+
       <el-form-item label="属性类型" prop="attrType">
-        <el-select v-model="dataForm.attrType" placeholder="请选择">
-          <el-option label="规格参数" :value="1"></el-option>
+        <el-select v-model="dataForm.attrType" placeholder="请选择属性类型">
+          <el-option label="基本属性" :value="1"></el-option>
           <el-option label="销售属性" :value="0"></el-option>
         </el-select>
       </el-form-item>
@@ -20,21 +27,21 @@
       <el-form-item label="值类型" prop="valueType">
         <el-switch
           v-model="dataForm.valueType"
-          active-text="允许多个值"
-          inactive-text="只能单个值"
           active-color="#13ce66"
           inactive-color="#ff4949"
+          active-text="允许多个值"
+          inactive-text="只能单个值"
           :inactive-value="0"
           :active-value="1"
-        ></el-switch>
+        >
+        </el-switch>
       </el-form-item>
       <el-form-item label="可选值" prop="valueSelect">
-        <!-- <el-input v-model="dataForm.valueSelect"></el-input> -->
         <el-select
           v-model="dataForm.valueSelect"
           multiple
-          filterable
           allow-create
+          filterable
           placeholder="请输入内容"
         ></el-select>
       </el-form-item>
@@ -42,10 +49,16 @@
         <el-input v-model="dataForm.icon" placeholder="属性图标"></el-input>
       </el-form-item>
       <el-form-item label="所属分类" prop="catelogId">
+        <!-- 2/34/225 手机的三级分类ID -->
         <category-cascader :catelogPath.sync="catelogPath"></category-cascader>
       </el-form-item>
+      <!-- type===1说明是新增 -->
       <el-form-item label="所属分组" prop="attrGroupId" v-if="type == 1">
-        <el-select ref="groupSelect" v-model="dataForm.attrGroupId" placeholder="请选择">
+        <el-select
+          ref="groupSelect"
+          v-model="dataForm.attrGroupId"
+          placeholder="请选择"
+        >
           <el-option
             v-for="item in attrGroups"
             :key="item.attrGroupId"
@@ -82,16 +95,23 @@
         ></el-switch>
       </el-form-item>
     </el-form>
-    <span slot="footer" class="dialog-footer">
-      <el-button @click="visible = false">取消</el-button>
-      <el-button type="primary" @click="dataFormSubmit()">确定</el-button>
-    </span>
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="visible = false">取 消</el-button>
+      <el-button type="primary" @click="dataFormSubmit()">确 定</el-button>
+    </div>
   </el-dialog>
 </template>
-
 <script>
-import CategoryCascader from "../common/category-cascader";
+import categoryCascader from "../common/category-cascader.vue";
 export default {
+  components: { categoryCascader },
+  // 父组件传过来的值 1 是新增 否则是修改
+  props: {
+    type: {
+      type: Number,
+      default: 1,
+    },
+  },
   data() {
     return {
       visible: false,
@@ -105,83 +125,77 @@ export default {
         attrType: 1,
         enable: 1,
         catelogId: "",
+        showDesc: 0,
         attrGroupId: "",
-        showDesc: 0
       },
       catelogPath: [],
       attrGroups: [],
       dataRule: {
         attrName: [
-          { required: true, message: "属性名不能为空", trigger: "blur" }
+          { required: true, message: "属性名不能为空", trigger: "blur" },
         ],
         searchType: [
           {
             required: true,
             message: "是否需要检索不能为空",
-            trigger: "blur"
-          }
+            trigger: "blur",
+          },
         ],
         valueType: [
           {
             required: true,
-            message: "值类型不能为空",
-            trigger: "blur"
-          }
+            message: "是否需要检索不能为空",
+            trigger: "blur",
+          },
         ],
         icon: [
-          { required: true, message: "属性图标不能为空", trigger: "blur" }
+          { required: true, message: "属性图标不能为空", trigger: "blur" },
         ],
         attrType: [
           {
             required: true,
             message: "属性类型不能为空",
-            trigger: "blur"
-          }
+            trigger: "blur",
+          },
         ],
         enable: [
           {
             required: true,
             message: "启用状态不能为空",
-            trigger: "blur"
-          }
+            trigger: "blur",
+          },
         ],
         catelogId: [
           {
             required: true,
             message: "需要选择正确的三级分类数据",
-            trigger: "blur"
-          }
+            trigger: "blur",
+          },
         ],
         showDesc: [
           {
             required: true,
             message: "快速展示不能为空",
-            trigger: "blur"
-          }
-        ]
-      }
+            trigger: "blur",
+          },
+        ],
+      },
     };
   },
-  props:{
-    type:{
-      type: Number,
-      default: 1
-    }
-  },
   watch: {
+    //监听到路径发生变化需要重新查询出这个三级分类的分组信息
     catelogPath(path) {
-      //监听到路径变化需要查出这个三级分类的分组信息
       console.log("路径变了", path);
       this.attrGroups = [];
       this.dataForm.attrGroupId = "";
-      this.dataForm.catelogId = path[path.length - 1];
+      this.dataForm.catelogId = [path.length - 1];
       if (path && path.length == 3) {
         this.$http({
           url: this.$http.adornUrl(
             `/product/attrgroup/list/${path[path.length - 1]}`
           ),
           method: "get",
-          params: this.$http.adornParams({ page: 1, limit: 10000000 })
+          params: this.$http.adornParams({ page: 1, limit: 10000000 }),
         }).then(({ data }) => {
           if (data && data.code === 0) {
             this.attrGroups = data.page.list;
@@ -195,9 +209,8 @@ export default {
         this.$message.error("请选择正确的分类");
         this.dataForm.catelogId = "";
       }
-    }
+    },
   },
-  components: { CategoryCascader },
   methods: {
     init(id) {
       this.dataForm.attrId = id || 0;
@@ -211,20 +224,19 @@ export default {
               `/product/attr/info/${this.dataForm.attrId}`
             ),
             method: "get",
-            params: this.$http.adornParams()
+            params: this.$http.adornParams(),
           }).then(({ data }) => {
             if (data && data.code === 0) {
               this.dataForm.attrName = data.attr.attrName;
               this.dataForm.searchType = data.attr.searchType;
               this.dataForm.valueType = data.attr.valueType;
               this.dataForm.icon = data.attr.icon;
-              this.dataForm.valueSelect = data.attr.valueSelect.split(";");
+              this.dataForm.valueSelect = data.attr.valueSelect;
               this.dataForm.attrType = data.attr.attrType;
               this.dataForm.enable = data.attr.enable;
-              this.dataForm.catelogId = data.attr.catelogId;
+              this.datForm.catelogId = data.attr.catelogId;
               this.dataForm.showDesc = data.attr.showDesc;
-              //attrGroupId
-              //catelogPath
+              //catelogPath attrGroupId
               this.catelogPath = data.attr.catelogPath;
               this.$nextTick(() => {
                 this.dataForm.attrGroupId = data.attr.attrGroupId;
@@ -236,11 +248,11 @@ export default {
     },
     // 表单提交
     dataFormSubmit() {
-      this.$refs["dataForm"].validate(valid => {
+      this.$refs["dataForm"].validate((valid) => {
         if (valid) {
           this.$http({
             url: this.$http.adornUrl(
-              `/product/attr/${!this.dataForm.attrId ? "save" : "update"}`
+              `/product/attr/${!this.dataForm.attrId} ? "save" : "update"`
             ),
             method: "post",
             data: this.$http.adornData({
@@ -249,13 +261,13 @@ export default {
               searchType: this.dataForm.searchType,
               valueType: this.dataForm.valueType,
               icon: this.dataForm.icon,
-              valueSelect: this.dataForm.valueSelect.join(";"),
+              valueSelect: this.dataForm.valueSelect,
               attrType: this.dataForm.attrType,
               enable: this.dataForm.enable,
               catelogId: this.dataForm.catelogId,
               attrGroupId: this.dataForm.attrGroupId,
-              showDesc: this.dataForm.showDesc
-            })
+              showDesc: this.dataForm.showDesc,
+            }),
           }).then(({ data }) => {
             if (data && data.code === 0) {
               this.$message({
@@ -265,7 +277,7 @@ export default {
                 onClose: () => {
                   this.visible = false;
                   this.$emit("refreshDataList");
-                }
+                },
               });
             } else {
               this.$message.error(data.msg);
@@ -274,10 +286,12 @@ export default {
         }
       });
     },
-    //dialogClose
+
+    // dialogClose
     dialogClose() {
       this.catelogPath = [];
-    }
-  }
+    },
+  },
 };
 </script>
+<sytle></sytle>
