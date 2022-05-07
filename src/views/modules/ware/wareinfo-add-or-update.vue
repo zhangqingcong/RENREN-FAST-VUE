d97c4e239dc4877777e076059ee61b7ca6d9c01b
zhangmingming <930137960@qq.com>
2022-05-08 00:37:44 +0800

    库存系统-商品库存模块开发完毕

---
+++ src/views/modules/ware/wareinfo-add-or-update.vue
@@ -0,0 +1,140 @@
+<!--  -->
+<template>
+  <el-dialog
+    :title="!dataForm.id ? '新增' : '修改'"
+    :close-on-click-modal="false"
+    :visible.sync="visible"
+  >
+    <el-form
+      :model="dataForm"
+      :rules="dataRule"
+      ref="dataForm"
+      @keyup.enter.native="dataFormSubmit()"
+      label-width="120px"
+    >
+      <el-form-item label="仓库名" prop="name">
+        <el-input v-model="dataForm.name" placeholder="仓库名"></el-input>
+      </el-form-item>
+      <el-form-item label="仓库地址" prop="address">
+        <el-input v-model="dataForm.address" placeholder="仓库地址"></el-input>
+      </el-form-item>
+      <el-form-item label="区域编码" prop="areacode">
+        <el-input v-model="dataForm.areacode" placeholder="区域编码"></el-input>
+      </el-form-item>
+    </el-form>
+    <span slot="footer" class="dialog-footer">
+      <el-button @click="visible = false">取消</el-button>
+      <el-button type="primary" @click="dataFormSubmit()">确定</el-button>
+    </span>
+  </el-dialog>
+</template>
+
+<script>
+//这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
+//例如：import 《组件名称》 from '《组件路径》';
+
+export default {
+  //import引入的组件需要注入到对象中才能使用
+  components: {},
+  data() {
+    //这里存放数据
+    return {
+      visible: false,
+      dataForm: {
+        id: 0,
+        name: "",
+        address: "",
+        areacode: "",
+      },
+      dataRule: {
+        name: [
+          {
+            required: true,
+            message: "仓库名不能为空",
+            trigger: "blur",
+          },
+        ],
+        address: [
+          { required: true, message: "仓库地址不能为空", trigger: "blur" },
+        ],
+        areacode: [
+          { required: true, message: "区域编码不能为空", trigger: "blur" },
+        ],
+      },
+    };
+  },
+  //监听属性 类似于data概念
+  computed: {},
+  //监控data中的数据变化
+  watch: {},
+  //方法集合
+  methods: {
+    init(id) {
+      this.dataForm.id = id || 0;
+      this.visible = true;
+      this.$nextTick(() => {
+        this.$refs["dataForm"].resetFields();
+        if (this.dataForm.id) {
+          this.$http({
+            url: this.$http.adornUrl(`/ware/wareinfo/info/${this.dataForm.id}`),
+            method: "get",
+            params: this.$http.adornParams(),
+          }).then(({ data }) => {
+            if (data && data.code === 0) {
+              this.dataForm.name = data.wareInfo.name;
+              this.dataForm.address = data.wareInfo.address;
+              this.dataForm.areacode = data.wareInfo.areacode;
+            }
+          });
+        }
+      });
+    },
+    //表单提交
+    dataFormSubmit() {
+      this.$refs["dataForm"].validate((valid) => {
+        if (valid) {
+          this.$http({
+            url: this.$http.adornUrl(
+              `/ware/wareinfo/${!this.dataForm.id ? "save" : "update"}`
+            ),
+            method: "post",
+            data: this.$http.adornData({
+              id: this.dataForm.id || undefined,
+              name: this.dataForm.name,
+              address: this.dataForm.address,
+              areacode: this.dataForm.areacode,
+            }),
+          }).then(({ data }) => {
+            if (data && data.code === 0) {
+              this.$message({
+                message: "操作成功",
+                type: "success",
+                duration: 1500,
+                onClose: () => {
+                  this.visible = false;
+                  this.$emit("refreshDataList");
+                },
+              });
+            } else {
+              this.$message.error(data.msg);
+            }
+          });
+        }
+      });
+    },
+  },
+  //生命周期 - 创建完成（可以访问当前this实例）
+  created() {},
+  //生命周期 - 挂载完成（可以访问DOM元素）
+  mounted() {},
+  beforeCreate() {}, //生命周期 - 创建之前
+  beforeMount() {}, //生命周期 - 挂载之前
+  beforeUpdate() {}, //生命周期 - 更新之前
+  updated() {}, //生命周期 - 更新之后
+  beforeDestroy() {}, //生命周期 - 销毁之前
+  destroyed() {}, //生命周期 - 销毁完成
+  activated() {}, //如果页面有keep-alive缓存功能，这个函数会触发
+};
+</script>
+<style scoped>
+</style>
